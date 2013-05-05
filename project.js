9227283298a3f97d524f4023d63cf2fde019f446
Philip Rideout <philiprideout@gmail.com>
2013-05-04 22:26:27 -0700

    use minified giza

--- project.js
+++ project.js
@@ -1,28 +1,13 @@
 (function() {
 
   var jqrypath = "http://ajax.googleapis.com/ajax/libs/";
-  var gizapath = "lib/giza/giza/";
 
   var scripts = [
     "lib/showdown.js",
     "lib/tween.js",
     "lib/seedrandom.js",
+    "lib/giza.min.js",
     jqrypath + "jquery/1.8.0/jquery.min.js",
-    gizapath + "Giza.js",
-    gizapath + "Utility.js",
-    gizapath + "Animation.js",
-    gizapath + "Shaders.js",
-    gizapath + "BufferView.js",
-    gizapath + "Vector.js",
-    gizapath + "Matrix.js",
-    gizapath + "Color.js",
-    gizapath + "Topo.js",
-    gizapath + "Polygon.js",
-    gizapath + "Surface.js",
-    gizapath + "Path.js",
-    gizapath + "Mouse.js",
-    gizapath + "Turntable.js",
-    gizapath + "Fullscreen.js",
     'config.js',
     'create.js',
     'centerline.js',
